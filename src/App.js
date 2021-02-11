d3c30ec4ae1b68645880ca648df50cdc77c615f6
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-02-11 12:04:38 +0900

    bootstrap core sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>    
     </div>
   );
 }
